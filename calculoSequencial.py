243f5dcb8b3dbbdece3aface079fb14941b4ba2c
lilianlima@brq.com <lilianlima@brq.com>
2019-09-20 10:50:17 -0300

    Add o .py

---
+++ calculoSequencial.py
@@ -0,0 +1,7 @@
+cont = 0
+seg = 0
+numero = int(input("Entre com o numero para ser calculado: "))
+while numero != cont:
+    cont = cont + 1
+    seg = seg + cont
+print(f'A soma dos numeros de {numero} até 1 é: {seg}')
